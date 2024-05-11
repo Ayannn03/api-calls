695d5e20161eecabefce4f6e89d516200f3ffafd
Ian Reniel R. Rey <127392923+Ayannn03@users.noreply.github.com>
2024-05-11 18:55:16 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import styleImport from 'vite-plugin-style-import';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    styleImport({
-      libs: [{
-        libraryName: 'axios',
-        esModule: true,
-        ensureStyleFile: true,
-        resolveStyle: (name) => {
-          return `axios/${name}/style`;
-        },
-      }],
-    }),
-  ],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['axios']
+    }
+  }
 });
